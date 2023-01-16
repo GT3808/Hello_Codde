f0e1c7b2e1362140f208b5d9fb61e52346b93494
Liudmila <topal.liudmila89@gmail.com>
2023-01-16 12:15:34 +0200

    Added text file

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+String username = Console.ReadLine();
+
+if(username == "Masa")
+{
+   Console.WriteLine("Wow, it's MASA");
+}
+else
+{
+   Console.Write("Hello, ");
+   Console.WriteLine(username);
+}
